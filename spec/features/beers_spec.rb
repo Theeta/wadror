cf5760edd5c06ae69cab4bc70c993ed9acb7fc2d
Noora Imberg <noppakotilo@gmail.com>
2014-02-07 01:33:35 +0200

    viikko 4 loppu

--- spec/features/beers_spec.rb
+++ spec/features/beers_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe "New Beer page" do
+  let!(:user) { FactoryGirl.create :user }
   before :each do
+    sign_in(username:"Pekka", password:"Foobar1")
     FactoryGirl.create :brewery
   end
   
